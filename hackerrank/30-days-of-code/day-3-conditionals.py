46c6f1bad326c33420d79450f75a4d3fe548cc02
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-04-25 20:26:56 +0000

    Create day-3-conditionals.py

---
+++ hackerrank/30-days-of-code/day-3-conditionals.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def check_weirdness(anumber):
+    is_odd = anumber % 2
+    if is_odd:
+        return "Weird"
+    elif 2<= anumber <= 5:
+        return "Not Weird"
+    elif 6<= anumber <= 20:
+        return "Weird"
+    else:
+        return "Not Weird"
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    print(check_weirdness(N))
