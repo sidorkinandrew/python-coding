ced192a50b41db34cd8fd55f244a3a03e6da7481
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-05-02 23:03:46 +0000

    Create day-09-recursion.py

---
+++ hackerrank/30-days-of-code/day-09-recursion.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/30-recursion/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def factorial(n):
+    # Write your code here
+    return math.factorial(n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
