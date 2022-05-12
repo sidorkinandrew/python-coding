09efed2753eb7a8c543db05be3672c0c9f358bd4
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-05-12 18:55:17 +0000

    Create  part_03_exam_02_task_02.py

---
+++ stepik.org_course_58852/part_03_exam_02_task_02.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/291000/step/2?unit=272483
+# put your python code here
+a,b = int(input()),int(input())
+print(f"Квадрат суммы {a} и {b} равен {(a+b)**2}")
+print(f"Сумма квадратов {a} и {b} равна {a**2+b**2}")
+
+
+
