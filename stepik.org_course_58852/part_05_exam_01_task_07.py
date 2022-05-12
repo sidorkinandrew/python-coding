3f4198a533fd0b2818516cfff211d6645666d727
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-05-12 18:59:48 +0000

    Create part_05_exam_01_task_07.py

---
+++ stepik.org_course_58852/part_05_exam_01_task_07.py
@@ -0,0 +1,9 @@
+# https://stepik.org/lesson/292172/step/7?unit=273659
+# put your python code here
+x1,y1,x2,y2 = int(input()),int(input()),int(input()),int(input())
+print("YES" if (abs(x1-x2)==1 and abs(y1-y2) == 2) or (abs(x1-x2)==2 and abs(y1-y2) == 1) else "NO")
+
+
+
+
+
