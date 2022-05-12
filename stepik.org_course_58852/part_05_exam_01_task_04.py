32c380263a897c6f6c6c40be3942f32047075678
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-05-12 18:58:14 +0000

    Create part_05_exam_01_task_04.py

---
+++ stepik.org_course_58852/part_05_exam_01_task_04.py
@@ -0,0 +1,9 @@
+# https://stepik.org/lesson/292172/step/4?unit=273659
+# put your python code here
+num = int(input())
+rome = {1:"I",2:"II",3:"III",4:"IV",5:"V",6:"VI",7:"VII",8:"VIII",9:"IX",10:"X"}
+print(rome.get(num,"ошибка"))
+
+
+
+
