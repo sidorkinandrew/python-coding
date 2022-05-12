0c946445061ca17fb8c3af6c4c009062a7841a8c
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-05-12 18:57:22 +0000

    Create part_05_exam_01_task_02.py

---
+++ stepik.org_course_58852/part_05_exam_01_task_02.py
@@ -0,0 +1,7 @@
+# https://stepik.org/lesson/292172/step/2?unit=273659
+# put your python code here
+x1,y1,x2,y2 = int(input()),int(input()),int(input()),int(input())
+print("YES" if (x1+y1)%2==(x2+y2)%2 else "NO")
+
+
+
