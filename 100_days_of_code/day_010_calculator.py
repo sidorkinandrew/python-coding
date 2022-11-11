e02af9eb3d2eae5287edd4250f3697c80a931b5a
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-11-11 00:08:46 +0000

    Create day_010_calculator.py

---
+++ 100_days_of_code/day_010_calculator.py
@@ -0,0 +1,64 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+from os import system
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  cycle = ""
+  while cycle != "q":
+    system('cls||clear')
+    print(logo)
+
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+      print(symbol)
+    should_continue = True
+ 
+    while should_continue:
+      operation_symbol = input("Pick an operation: ")
+      num2 = float(input("What's the next number?: "))
+      calculation_function = operations[operation_symbol]
+      answer = calculation_function(num1, num2)
+      print(f"{num1} {operation_symbol} {num2} = {answer}")
+      
+      cycle=input(f"Type 'y' to continue calculating with {answer},\nor type 'n' to start a new calculation,\nor type 'q' to quit the program: ")
+      if cycle == 'y':
+        num1 = answer
+      else:
+        should_continue = False
+
+
+calculator()
