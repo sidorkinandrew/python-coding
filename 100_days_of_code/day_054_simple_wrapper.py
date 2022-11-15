70ebc8f1e99c034431937fa95045daaa056cfcd8
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-11-15 22:32:08 +0000

    Update day_054_simple_wrapper.py

--- 100_days_of_code/day_054_simple_wrapper.py
+++ 100_days_of_code/day_054_simple_wrapper.py
@@ -28,3 +28,20 @@ def slow_function():
 
 fast_function()
 slow_function()
+
+
+# HTML simple wrappers
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
