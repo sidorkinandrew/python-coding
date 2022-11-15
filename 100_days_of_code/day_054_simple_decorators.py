6a78c6a4dc5e44ec5073e5d289a162ab717e8db4
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-11-15 22:43:45 +0000

    Update and rename day_054_simple_wrapper.py to day_054_simple_decorators.py

--- 100_days_of_code/day_054_simple_decorators.py
+++ 100_days_of_code/day_054_simple_decorators.py
@@ -45,3 +45,20 @@ def make_underlined(function):
     def wrapper():
         return "<u>" + function() + "</u>"
     return wrapper
+
+
+# Simple logging decorator
+
+def logging_decorator(fn):
+    def wrapper(*args, **kwargs):
+        print(f"You called {fn.__name__}{args}")
+        result = fn(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+    
+a_function(1, 2, 3)
