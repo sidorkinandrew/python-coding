73600b445716225b3c8326b0bb3b36b16328115a
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2022-11-15 18:01:20 +0000

    Update day_049_Selenium_LinkedIn_EasyApply_resume_submitter.py

--- 100_days_of_code/day_049_Selenium_LinkedIn_EasyApply_resume_submitter.py
+++ 100_days_of_code/day_049_Selenium_LinkedIn_EasyApply_resume_submitter.py
@@ -1,6 +1,9 @@
 import os
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
@@ -9,9 +12,14 @@ LINKEDIN_ACCOUNT_EMAIL = os.environ['LINKEDIN_ACCOUNT_EMAIL']
 LINKEDIN_ACCOUNT_PASSWORD = os.environ['LINKEDIN_ACCOUNT_PASSWORD']
 MY_PHONE_NUMBER = os.environ['MY_PHONE_NUMBER']
 
-chrome_driver_path = "/Users/angela/Development/chromedriver"
-driver = webdriver.Chrome(chrome_driver_path)
+chrome_driver_path = "/temp/chromedriver"
+
+options = Options()
+service = Service(chrome_driver_path)
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=service, options=options)
 driver.maximize_window()
+
 driver.get(
     "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
 
@@ -27,17 +35,51 @@ password_field = driver.find_element(By.ID, "password")
 password_field.send_keys(LINKEDIN_ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-# Locate the apply button
-time.sleep(5)
-apply_button = driver.find_element(By.CSS_SELECTOR, ".jobs-s-apply button")
-apply_button.click()
+password_field.send_keys(Keys.ENTER)
 
-# If application requires phone number and the field is empty, then fill in the number.
 time.sleep(5)
-phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
-if phone.text == "":
-    phone.send_keys(MY_PHONE_NUMBER)
 
-# Submit the application
-submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
-submit_button.click()
+all_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+
+for listing in all_listings:
+    print("called")
+    listing.click()
+    time.sleep(2)
+
+    # Try to locate the apply button, if can't locate then skip the job.
+    try:
+        apply_button = driver.find_element(By.CSS_SELECTOR, ".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        # If phone field is empty, then fill your phone number.
+        phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(MY_PHONE_NUMBER)
+
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+
+        # If the submit_button is a "Next" button, then this is a multistep application, so skip.
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+
+        # Once application completed, close the pop-up window.
+        time.sleep(2)
+        close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+        close_button.click()
+
+    # If already applied to job or job is no longer accepting applications, then skip.
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
+driver.quit()
